a401b927362d8472fbdeff08af4513cb3d2f37c5
hatanvir <tanvirhasan553@gmail.com>
2018-11-15 21:45:10 +0600

    Updated

--- app/src/main/java/com/example/tanvir/reportbook/Fragments/TodayFragment.java
+++ app/src/main/java/com/example/tanvir/reportbook/Fragments/TodayFragment.java
@@ -1,5 +1,6 @@
 package com.example.tanvir.reportbook.Fragments;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -26,11 +27,17 @@ import com.example.tanvir.reportbook.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.zip.Inflater;
 
 public class TodayFragment extends Fragment {
     TextView prayerCountTv,quranTv,hadithTv,academicTv,novelTv,newspaperTv,skillTv,marketingTv,wakeUpTv,sleepTv,workOutTv;
     TextView dateTv;
+    ImageView prevImage,nextImg;
     DatabaseManager databaseManager;
+    LinearLayout detailsViewIdLenearLayout;
+    FloatingActionButton fab;
+    ViewGroup container;
+
     String customDate;
     TodayFragment todayFragment;
     String currentDate;
@@ -52,9 +59,10 @@ public class TodayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         databaseManager = new DatabaseManager(getActivity());
+        this.container = container;
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_todays, container, false);
-
+        //viewGroup = view.findViewById(R.id.detailsViewIdLinearLayout);
         viewInitialization(view);
 
         //current time picking from calender
@@ -67,7 +75,8 @@ public class TodayFragment extends Fragment {
         currentDate = currentDay+" "+monthName[Integer.parseInt(currentMOnth)-1]+" "+currentYear;
 
         dateTv.setText(currentDate);
-        //ArrayList<ReportDetails> arrayList = new ArrayList<>();
+        nextImg.setVisibility(View.GONE);
+
         final ReportDetails reportDetails  = databaseManager.getREportById(Integer.parseInt(dateId));
         try{
             //text view formation
@@ -76,7 +85,7 @@ public class TodayFragment extends Fragment {
             }else {
                 prayerCountTv.setText(reportDetails.getPrayer()+" times");
             }
-            //prayerCountTv.setText(reportDetails.getPrayer()+" times");
+
             if(reportDetails.getQuran().equals("1") || reportDetails.getQuran().equals("0")){
                 quranTv.setText(reportDetails.getQuran()+" page");
             }else {
@@ -100,7 +109,8 @@ public class TodayFragment extends Fragment {
         }catch (Exception e){
 
         }
-        FloatingActionButton fab = view.findViewById(R.id.fab);
+
+        //fab behaviour
         if(currentDate.equals(currentDate)){
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -132,14 +142,53 @@ public class TodayFragment extends Fragment {
         return view;
     }
 
-    public Fragment fragmentLoading (){
-        //layout click listener
+    public void fragmentLoading () {
+
+        Calendar calendar = Calendar.getInstance();
         final boolean[] dateFindOrNotIndicator = {false};
+
+        //previous button behaviour on dateview
+        prevImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dbId;
+                calendar.add(Calendar.DATE, -1);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                final int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                if (String.valueOf(month + 1).length() == 1) {
+                    dbId = String.valueOf(day) + "0" + String.valueOf(month) + String.valueOf(year);
+                } else {
+                    dbId = String.valueOf(day) + String.valueOf(month) + String.valueOf(year);
+                }
+
+                settingValueToView(dbId, day, month, year,calendar);
+            }
+        });
+
+        nextImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dbId;
+                calendar.add(Calendar.DATE, 1);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                final int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                if (String.valueOf(month + 1).length() == 1) {
+                    dbId = String.valueOf(day) + "0" + String.valueOf(month) + String.valueOf(year);
+                } else {
+                    dbId = String.valueOf(day) + String.valueOf(month) + String.valueOf(year);
+                }
+
+                settingValueToView(dbId, day, month, year,calendar);
+            }
+        });
+        //layout click listener
         dateTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar calendar = Calendar.getInstance();
-                //.set(2018,10,29);
                 int day = calendar.get(Calendar.DAY_OF_MONTH);
                 final int month = calendar.get(Calendar.MONTH);
                 int year = calendar.get(Calendar.YEAR);
@@ -147,52 +196,97 @@ public class TodayFragment extends Fragment {
                     @Override
                     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
                         String dbId;
-
-                        if (String.valueOf(i1+1).length()==1){
-                            dbId = String.valueOf(i2)+"0"+String.valueOf(i1)+String.valueOf(i);
-                        }else {
-                            dbId = String.valueOf(i2)+String.valueOf(i1)+String.valueOf(i);
+                        if (String.valueOf(i1 + 1).length() == 1) {
+                            dbId = String.valueOf(i2) + "0" + String.valueOf(i1) + String.valueOf(i);
+                        } else {
+                            dbId = String.valueOf(i2) + String.valueOf(i1) + String.valueOf(i);
                         }
-                        Toast.makeText(getActivity(), ""+dbId, Toast.LENGTH_SHORT).show();
+                        settingValueToView(dbId,i2,i1,i,calendar);
+                        Toast.makeText(getActivity(), "" + dbId, Toast.LENGTH_SHORT).show();
 
-                        if(databaseManager.checkIdExistOrNot(Integer.parseInt(dbId))==true){
-                            //Toast.makeText(getActivity(), "Db found"+dbId, Toast.LENGTH_SHORT).show();
-                            ReportDetails reportDetails1 = databaseManager.getREportById(Integer.parseInt(dbId));
-                            prayerCountTv.setText(reportDetails1.getPrayer()+" times");
-                            quranTv.setText(reportDetails1.getQuran());
-                            hadithTv.setText(reportDetails1.getHadith());
-                            academicTv.setText(reportDetails1.getAcademicBook());
-                            novelTv.setText(reportDetails1.getNovelOrOtherBook());
-                            newspaperTv.setText(reportDetails1.getNewspaper());
-                            skillTv.setText(reportDetails1.getSkillDev());
-                            marketingTv.setText(reportDetails1.getMarketing());
-                            wakeUpTv.setText(reportDetails1.getWakeupTime());
-                            sleepTv.setText(reportDetails1.getSleepTime());
-                            workOutTv.setText(reportDetails1.getWorkout());
-                            Toast.makeText(getActivity(), "Yes", Toast.LENGTH_SHORT).show();
-                            customDate = String.valueOf(i2)+" "+monthName[i1]+" "+String.valueOf(i);
-                            dateTv.setText(currentDate);
-                            //todayFragment = new TodayFragment();
-                            dateFindOrNotIndicator[0] = true;
-
-                        }else {
-                            Toast.makeText(getActivity(), "You don't have enter any report in this date. "+dbId, Toast.LENGTH_SHORT).show();
-                        }
                     }
                 }, year, month, day);
                 datePickerDialog.show();
 
             }
         });
-        if (dateFindOrNotIndicator[0]==true){
+        /*if (dateFindOrNotIndicator[0]==true){
             TodayFragment todayFragment = new TodayFragment();
             return todayFragment;
         }else {
             return null;
+        }*/
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void settingValueToView(String dbId, int day, int month, int year, Calendar calendar){
+        Calendar newCalender = Calendar.getInstance();
+
+        int day1 = newCalender.get(Calendar.DAY_OF_MONTH);
+        final int month1 = newCalender.get(Calendar.MONTH);
+        int year1 = newCalender.get(Calendar.YEAR);
+
+        String dbIdTemp;
+        if (String.valueOf(day + 1).length() == 1) {
+            dbIdTemp = String.valueOf(day1) + "0" + String.valueOf(month1) + String.valueOf(year1);
+        } else {
+            dbIdTemp = String.valueOf(day1) + String.valueOf(month1) + String.valueOf(year1);
+        }
+        if(databaseManager.checkIdExistOrNot(Integer.parseInt(dbId))==true && dbIdTemp.equals(dbId)){
+            ReportDetails reportDetails1 = databaseManager.getREportById(Integer.parseInt(dbId));
+            prayerCountTv.setText(reportDetails1.getPrayer()+" times");
+            quranTv.setText(reportDetails1.getQuran());
+            hadithTv.setText(reportDetails1.getHadith());
+            academicTv.setText(reportDetails1.getAcademicBook());
+            novelTv.setText(reportDetails1.getNovelOrOtherBook());
+            newspaperTv.setText(reportDetails1.getNewspaper());
+            skillTv.setText(reportDetails1.getSkillDev());
+            marketingTv.setText(reportDetails1.getMarketing());
+            wakeUpTv.setText(reportDetails1.getWakeupTime());
+            sleepTv.setText(reportDetails1.getSleepTime());
+            workOutTv.setText(reportDetails1.getWorkout());
+            Toast.makeText(getActivity(), "Yes", Toast.LENGTH_SHORT).show();
+            customDate = String.valueOf(day)+" "+monthName[month]+" "+String.valueOf(year);
+            dateTv.setText(currentDate);
+
+            nextImg.setVisibility(View.INVISIBLE);
+            detailsViewIdLenearLayout.setVisibility(View.VISIBLE);
+            fab.setVisibility(View.VISIBLE);
+
+        }else if(databaseManager.checkIdExistOrNot(Integer.parseInt(dbId))==true &&  !dbIdTemp.equals(dbId)){
+            ReportDetails reportDetails1 = databaseManager.getREportById(Integer.parseInt(dbId));
+            prayerCountTv.setText(reportDetails1.getPrayer()+" times");
+            quranTv.setText(reportDetails1.getQuran());
+            hadithTv.setText(reportDetails1.getHadith());
+            academicTv.setText(reportDetails1.getAcademicBook());
+            novelTv.setText(reportDetails1.getNovelOrOtherBook());
+            newspaperTv.setText(reportDetails1.getNewspaper());
+            skillTv.setText(reportDetails1.getSkillDev());
+            marketingTv.setText(reportDetails1.getMarketing());
+            wakeUpTv.setText(reportDetails1.getWakeupTime());
+            sleepTv.setText(reportDetails1.getSleepTime());
+            workOutTv.setText(reportDetails1.getWorkout());
+            Toast.makeText(getActivity(), "Yes", Toast.LENGTH_SHORT).show();
+            customDate = String.valueOf(day)+" "+monthName[month]+" "+String.valueOf(year);
+            dateTv.setText(currentDate);
+
+            nextImg.setVisibility(View.VISIBLE);
+            detailsViewIdLenearLayout.setVisibility(View.VISIBLE);
+            fab.setVisibility(View.VISIBLE);
+        }
+        else {
+            dateTv.setText(String.valueOf(day)+" "+monthName[month]+" "+String.valueOf(year));
+            Toast.makeText(getActivity(), "You don't have enter any report in this date. "+dbId+"  "+currentDate+" "+day, Toast.LENGTH_SHORT).show();
+            detailsViewIdLenearLayout.setVisibility(View.INVISIBLE);
+            nextImg.setVisibility(View.VISIBLE);
+            fab.setVisibility(View.INVISIBLE);
+
         }
     }
 
     public void viewInitialization(View view){
+
+        fab = view.findViewById(R.id.fab);
 
         prayerCountTv = view.findViewById(R.id.prayerCount);
         quranTv = view.findViewById(R.id.quranRecDurTv);
@@ -207,5 +301,9 @@ public class TodayFragment extends Fragment {
         workOutTv = view.findViewById(R.id.workOutDurTv);
 
         dateTv = view.findViewById(R.id.linearLayoutBt);
+        prevImage = view.findViewById(R.id.prevImg);
+        nextImg = view.findViewById(R.id.nextImg);
+
+        detailsViewIdLenearLayout = view.findViewById(R.id.detailsViewIdLinearLayout);
     }
 }
