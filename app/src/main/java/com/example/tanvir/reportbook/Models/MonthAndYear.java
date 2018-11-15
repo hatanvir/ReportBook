b1b90c84e54c82eb23d86eb9dee776701daaa3b9
hatanvir <tanvirhasan553@gmail.com>
2018-11-15 21:41:27 +0600

    MonthAndYear model class added

---
+++ app/src/main/java/com/example/tanvir/reportbook/Models/MonthAndYear.java
@@ -0,0 +1,18 @@
+package com.example.tanvir.reportbook.Models;
+
+public class MonthAndYear {
+    private String month,year;
+
+    public MonthAndYear(String month, String year) {
+        this.month = month;
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
